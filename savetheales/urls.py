eabadde2b00bf39c48a5a8eaaba10342afb5cb1a
Aidai Tazabekova <aidaitazabekova@gmail.com>
2021-06-16 20:37:37 +0600

    cmcmc

--- savetheales/urls.py
+++ savetheales/urls.py
@@ -1,21 +1,8 @@
-"""savetheales URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from products.views import CategoryView, ProductView
 from about.views import ContactView
 # from order.views import OrderView
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -32,3 +19,5 @@ urlpatterns = [
     # path('order', OrderView.as_view({'get':'list'})),
     # path('order/create', OrderView.as_view({'post':'create'})
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
