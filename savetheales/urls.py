9965052e7a80cbe3515c90ac939d29944dbcf93e
Aidai Tazabekova <aidaitazabekova@gmail.com>
2021-06-18 19:51:44 +0600

    ada

--- savetheales/urls.py
+++ savetheales/urls.py
@@ -10,6 +10,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('contacts', ContactView.as_view({'get':'list'})),
     path('category', CategoryView.as_view({'get':'list'})),
