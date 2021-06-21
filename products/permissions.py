613b50a0ea09564ea99b72bbeaf67e84932b4777
Aidai Tazabekova <aidaitazabekova@gmail.com>
2021-06-21 19:23:33 +0600

    fd

--- products/permissions.py
+++ products/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOmly(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_staff or request == 'GET':
+        if request.user.is_staff or request.method == 'GET':
             return True
         else:
             return False
